4182a9bf9daaac43b0ed3635619685eeccf667e5
ianaroot <ianaroot@gmail.com>
2017-07-20 14:05:58 -0400

    bug fix rules.js change means of accessing pieceControllers

--- rules.js
+++ rules.js
@@ -1,4 +1,6 @@
-var Rules = (function () {
+// pieceControllerSet must be defined by the gameController when creating Rules singleton, not the best approach,
+// but there were lots of arguments on the internet about dependency injection and singltons in js, and i just wanted something to work. will address later
+var Rules = function () {
   var instance = {
     moveIsIllegal: function(startPosition, endPosition, board){
       var layOut = board.layOut,
@@ -7,44 +9,50 @@ var Rules = (function () {
         pieceType = pieceString.substring(5, stringLength),
         team = board.teamAt(startPosition);
       if( pieceType === "Pawn" ){ pieceType = pieceString.charAt(0).toUpperCase() + pieceString.slice(1) }
-      pieceController = new window[ pieceType + "Controller"]()
+      pieceController = this.pieceControllerSet[pieceType]
       illegal = false
+      // debugger
       if ( !Board.classMethods.inBounds(endPosition) ){
         alert('stay on the board, fool')
         illegal = true
 
-      } else if( !pieceController.positionViable( {startPosition: startPosition, endPosition: endPosition, layOut: layOut} ) ) {//( !pieceController.positionIsInPaths({position: position, endPosition: endPosition, board: board}) ){
+      } else if( !pieceController.positionViable( {startPosition: startPosition, endPosition: endPosition, layOut: layOut} ) ) {
+      // else if( !pieceController.positionIsInPaths({position: position, endPosition: endPosition, board: board}) ){
 
         alert("that's not how that piece moves")
         illegal = true
       } else if( board.positionIsOccupiedByTeamMate(endPosition, team ) ){
         alert("what, are you trying to capture your own piece?")
         illegal = true
-      } //else if( this.kingCheck( {piece: piece, position: endPosition})){
-      //   alert("check yo")
+      } //else if( this.kingCheck( {piece: startPosition, endPosition: endPosition, board: board})){
+      //   alert("check yo king fool")
       //   illegal = true
       // }
       return illegal
     },
     kingCheck: function(args){
-      var position          = args["position"],
-          piece             = args["piece"]
-          pieceCopy         = args["piece"],
-          team              = pieceCopy.team,
-          activeTeamPieces  = team.activePieces,
-          king              = activeTeamPieces.king,
-          tilesCopy         = board.deepCopyTiles,
+      var startPosition     = args["startPosition"],
+          endPosition       = args["endPosition"]
+          board             = args["board"],
+          teamString        = board.teamAt[startPosition],
+          kingPosition      = board.kingPosition(teamString)
+          // activeTeamPieces  = team.activePieces,
           danger            = false,
           opposingTeam;
-      if( team === white ){
-        opposingTeam = black
+      if( teamString === "white" ){
+        opposingTeamString = "black"
       } else {
-        opposingTeam = white
+        opposingTeamString = "white"
       };
-      var activeOpposingTeamPieces = opposingTeam.activePieces;
-      for (var i = 0; i < activeOpposingTeamPieces.length; i++){
-        if( isAttackedBy({piece: activeOpposingTeamPieces[i], position: kingPosition}) ){ danger = true }
-      }
+      // var activeOpposingTeamPieces = opposingTeam.activePieces;
+      // for (var i = 0; i < activeOpposingTeamPieces.length; i++){
+      //   if( isAttackedBy({piece: activeOpposingTeamPieces[i], position: kingPosition}) ){ danger = true }
+      // }
+
+
+
+
+
       // danger = board.isAttacked({position: position, piece: pieceCopy, tiles: tilesCopy});
       return danger
       // pretend king has all movement abilities. stretch outward with them until hittting block, see if that block has the ability that was used to get to the king,
@@ -60,11 +68,11 @@ var Rules = (function () {
       return object;
   }
   return {
-    getInstance: function() {
+    getInstance: function(controllerSet) {
       if (!instance) {
-        instance = createInstance();
+        instance = createInstance(controllerSet);
       }
       return instance;
     },
   };
-})();
+}();
