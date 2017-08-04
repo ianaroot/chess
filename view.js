f8499f9f74040ba95c005042b5d1abab04986970
ianaroot <ianaroot@gmail.com>
2017-08-04 18:42:01 -0400

    view.js scope cleanup

--- view.js
+++ view.js
@@ -34,7 +34,7 @@ var View = (function(){
     pieceInitials: function(string){
       var firstInitial = string[0],
         secondInitial;
-      for (i = 0; i < string.length; i++){
+      for (var i = 0; i < string.length; i++){
         if( string[i] === string[i].toUpperCase() ){ secondInitial = string[i] }
       };
       return firstInitial + secondInitial
